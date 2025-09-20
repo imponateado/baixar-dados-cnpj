7883e1a50d5b595aad3b34466c0f0210880d0e11
Leonardo Teodoro <38961152+imponateado@users.noreply.github.com>
2025-09-20 15:29:46 -0300

    fix typo

--- baixar-dados-estabelecimentos-low-memory.sh
+++ baixar-dados-estabelecimentos-low-memory.sh
@@ -19,7 +19,7 @@ for i in {0..9}; do
 
     files=(*.ESTABELE);
     if [ -e "${files[0]}" ]; then
-        clickhouse-local --query "SELECT c1 as cnpj_basico,c2 as cnpj_ordem,c3 as cnpj_dv,c4 as identificador,c5 as nome_fantasia,c6 as situacao_cadastral,c7 as data_situacao_cadastral,c8 as motivo_situacao_cadastral,c9 as nome_cidade_exterior,c10 as pais,c11 as data_inicio_atividade,c12 as cnae_principal,c13 as cnaes_secundarios,c14 as tipo_logradouro,c15 as lougradouro,c16 as numero,c17 as complemento,c18 as bairro,c19 as cep,c20 as uf,c21 as municipio,c22 as ddd_1,c23 as telefone_1,c24 as ddd_2,c25 as telefone_2,c26 as ddd_fax,c27 as fax,c28 as email,c29 as situacao_especial,c30 as data_situacao_especial FROM file('*.ESTABELE') INTO OUTFILE 'estabs_${i}.parquet' SETTINGS format_csv_delimiter=';', output_format_parquet_compression_method = 'zstd'";
+        clickhouse-local --query "SELECT c1 as cnpj_basico,c2 as cnpj_ordem,c3 as cnpj_dv,c4 as identificador,c5 as nome_fantasia,c6 as situacao_cadastral,c7 as data_situacao_cadastral,c8 as motivo_situacao_cadastral,c9 as nome_cidade_exterior,c10 as pais,c11 as data_inicio_atividade,c12 as cnae_principal,c13 as cnaes_secundarios,c14 as tipo_logradouro,c15 as logradouro,c16 as numero,c17 as complemento,c18 as bairro,c19 as cep,c20 as uf,c21 as municipio,c22 as ddd_1,c23 as telefone_1,c24 as ddd_2,c25 as telefone_2,c26 as ddd_fax,c27 as fax,c28 as email,c29 as situacao_especial,c30 as data_situacao_especial FROM file('*.ESTABELE') INTO OUTFILE 'estabs_${i}.parquet' SETTINGS format_csv_delimiter=';', output_format_parquet_compression_method = 'zstd'";
         rm *.ESTABELE;
         unset files;
     else
